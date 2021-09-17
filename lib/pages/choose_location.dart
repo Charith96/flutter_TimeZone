5e7eb834e58a544c342372596dfc9591eb692a16
Charith <wcharithbuddhipriya@gmail.com>
2021-09-17 18:48:44 +0530

    add Appbar to chooselocation.dart

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wold_timezone_app/pages/home.dart';
 
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({Key key}) : super(key: key);
@@ -11,6 +12,17 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          tooltip: 'Back to Home',
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose Location'),
+        elevation: 0,
+      ),
       body: Text('Choose location Screen'),
     );
   }
